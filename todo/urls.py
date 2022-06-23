3b5fb3206fb118e29ecea68d2feaaf43ec448668
stanzleey <stanleychenho@gmail.com>
2022-06-23 15:57:24 +0700

    step 3

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-# import my_view from todo Application
-from .views import my_view
+# import View dari todo Application
+from .views import index_view, detail_view
 
+app_name = 'todo'
 urlpatterns = [
-    path('', my_view, name='my_view')
+    # route untuk halaman daftar task
+    path('', index_view, name='index'),
+  	
+# route untuk halaman detail task
+    path('<int:task_id>', detail_view, name='detail')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 
-def my_view(request):
-    return HttpResponse("Belajar CRUD Django")
-# Create your views here.
+# import class Task dari file todo/models.py
+from .models import Task
+
+
+# Membuat View untuk halaman daftar task
+def index_view(request):
+    # Mengambil semua data task
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    # parsing data task ke template todo/index.html dan merender nya
+    return render(request, 'todo/index.html', context)
+
+
+# Membuat View untuk halaman detail task
+def detail_view(request, task_id):
+    # Mengambil data task berdasarkan task ID
+    try:
+        task = Task.objects.get(pk=task_id)
+        context = {
+            'task': task
+        }
+    except Task.DoesNotExist:
+        # Jika data task tidak ditemukan,
+        # maka akan di redirect ke halaman 404 (Page not found).
+        raise Http404("Task tidak ditemukan.")
+    # parsing data task ke template todo/detail.html dan merendernya
+    return render(request, 'todo/detail.html', context)
